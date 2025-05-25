815efa9e39e4e22cce93c5c0846799844bbdec8d
Divyam Shah <divyamshah1234@gmail.com>
2025-05-26 00:25:44 +0530

    updated frontend and developed timeslot

--- ovenfresh_ecommerce/FrontEnd/views.py
+++ ovenfresh_ecommerce/FrontEnd/views.py
@@ -48,3 +48,12 @@ class AdminPincodeViewSet(viewsets.ViewSet):
     @check_authentication(required_role="admin")
     def list(self, request):
         return render(request, 'admin/admin_pincode_manager.html')
+
+
+class AdminTimeslotViewSet(viewsets.ViewSet):
+
+    @handle_exceptions
+    @check_authentication(required_role="admin")
+    def list(self, request):
+        return render(request, 'admin/admin_timeslots_manager.html')
+
