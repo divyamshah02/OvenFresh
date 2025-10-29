5c1176f7a1cad36d6c31e4d81a3ffaee66409246
Divyam Shah <divyamshah1234@gmail.com>
2025-10-29 10:31:54 +0530

    solved issue

--- ovenfresh_ecommerce/ovenfresh_ecommerce/middleware.py
+++ ovenfresh_ecommerce/ovenfresh_ecommerce/middleware.py
@@ -18,20 +18,20 @@ class FrontendRewriteMiddleware:
         r"|django-admin|static|media|sitemap\.xml|robots\.txt)"
     )
 
-    # FRONTEND_ALLOWED_REGEX = re.compile(
-    #     r"^(shop|payment-success-callback|refund-policy|contact-us|product|cart|checkout|order-success|order-detail|account"
-    #     r"|admin-login|admin-logout|admin-dashboard|admin-home-cms|admin-cms|admin-products"
-    #     r"|admin-add-product|admin-category|admin-pincodes|admin-timeslot|admin-coupon"
-    #     r"|admin-delivery-person|admin-todays-delivery-orders|admin-all-confirmed-orders|admin-all-orders|admin-add-order|admin-order-detail"
-    #     r"|admin-pincode-order|admin-review|delivery-login|delivery-dashboard|admin-product-tax-rates)"
-    # )
     FRONTEND_ALLOWED_REGEX = re.compile(
-        r"^(payment-success-callback|refund-policy|order-success|order-detail|account"
+        r"^(shop|payment-success-callback|refund-policy|contact-us|product|cart|checkout|order-success|order-detail|account"
         r"|admin-login|admin-logout|admin-dashboard|admin-home-cms|admin-cms|admin-products"
         r"|admin-add-product|admin-category|admin-pincodes|admin-timeslot|admin-coupon"
         r"|admin-delivery-person|admin-todays-delivery-orders|admin-all-confirmed-orders|admin-all-orders|admin-add-order|admin-order-detail"
         r"|admin-pincode-order|admin-review|delivery-login|delivery-dashboard|admin-product-tax-rates)"
     )
+    # FRONTEND_ALLOWED_REGEX = re.compile(
+    #     r"^(payment-success-callback|refund-policy|order-success|order-detail|account"
+    #     r"|admin-login|admin-logout|admin-dashboard|admin-home-cms|admin-cms|admin-products"
+    #     r"|admin-add-product|admin-category|admin-pincodes|admin-timeslot|admin-coupon"
+    #     r"|admin-delivery-person|admin-todays-delivery-orders|admin-all-confirmed-orders|admin-all-orders|admin-add-order|admin-order-detail"
+    #     r"|admin-pincode-order|admin-review|delivery-login|delivery-dashboard|admin-product-tax-rates)"
+    # )
 
     def __init__(self, get_response):
         self.get_response = get_response
